cfad9ffea2bb78841a4db4d07ff2da43bf8b14bd
user <user@server2.benzin.local>
2016-11-29 22:58:50 +0500

    push to github

---
+++ src/base.js
@@ -0,0 +1,10 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+  logname: process.env.LOGNAME,
+  apiKey: "AIzaSyDZ63d2icOseI9aDASpeQnmw_cD-OotvEk",
+  authDomain: "catch-the-day-6059.firebaseapp.com",
+  databaseURL: "https://catch-day-60859.firebaseio.com",
+});
+
+export default base;
